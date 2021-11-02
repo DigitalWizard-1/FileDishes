224138ac632dcddb2fe74d10958aac66aed28d8a
Alexey Voylukov <dwg_dwg@mail.ru>
2021-11-02 17:43:27 +0300

    Second version. Many def))

--- file.py
+++ file.py
@@ -1,4 +1,3 @@
-import os
 from pprint import pprint
 
 with open('recipes.txt',encoding='utf-8') as file:
@@ -13,30 +12,25 @@ with open('recipes.txt',encoding='utf-8') as file:
         cook_book[dish_name] = temp_data
         file.readline()
 
+def add_product(list_product,product,num):
+    list_product[product['ingredient_name']] = {'measure': product['measure'], 'quantity': product['quantity'] * num}
+
+def add_products(list_product,product,num):
+    tmp = list_product[product['ingredient_name']]
+    tmp.update(quantity=tmp['quantity'] + product['quantity'] * num)
+    list_product[product['ingredient_name']] = tmp
+
 def get_shop_list_by_dishes(dishes, person_count):
     shopping_list = {}
     for dish_id in dishes:
-        # pprint(cook_book[dish_id])
-        # print(dish_id)
         for ing in cook_book[dish_id]:
             if shopping_list == {}:
-                shopping_list[ing['ingredient_name']] = {'measure':ing['measure'],'quantity':ing['quantity']*person_count}
-                # print('Самое первая запись',shopping_list)
+                add_product(shopping_list,ing,person_count)
             elif ing['ingredient_name'] in shopping_list:
-                # print('уже есть')
-                tmp = shopping_list[ing['ingredient_name']]
-                # print(tmp)
-                tmp.update(quantity=tmp['quantity'] + ing['quantity']*person_count)
-                # print(tmp)
-                # print(ing['ingredient_name'])
-                shopping_list[ing['ingredient_name']] = tmp
-                # {'measure':ing['measure'],'quantity':ing['quantity']*person_count})
+                add_products(shopping_list,ing,person_count)
             else:
-                # print("добавим еще")
-                shopping_list[ing['ingredient_name']] = {'measure':ing['measure'],'quantity':ing['quantity']*person_count}
-                # print(shopping_list)
+                add_product(shopping_list,ing,person_count)
     return shopping_list
-
 
 zakaz = get_shop_list_by_dishes(['Омлет','Омлет', 'Омлет', 'Фахитос'], 4)
 pprint(zakaz)
